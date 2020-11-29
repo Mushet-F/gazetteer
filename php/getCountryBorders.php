241a046aee5d906373f13e37d41a64882372c05e
Mushet F <64835634+Mushet-F@users.noreply.github.com>
2020-11-29 12:56:20 +0000

    Update getCountryBorders.php
    change $url

--- php/getCountryBorders.php
+++ php/getCountryBorders.php
@@ -2,7 +2,13 @@
 
 	$executionStartTime = microtime(true) / 1000;
 	
-    $url = 'http://localhost/gazetteer/data/countryBorders.geo.json';
+    	$uri = $_SERVER['REQUEST_URI'];
+	
+	$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+	
+	$tempurl = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+	$url = str_replace('php/getCountryBorders.php', 'data/countryBorders.geo.json', $tempurl);
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
